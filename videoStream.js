b4ec91f902bb8a2c2f49708f09a6abbf005da838
Muhammad Yunus <muhammadyunusiot@gmail.com>
2024-09-16 15:38:35 +0700

    update

--- videoStream.js
+++ videoStream.js
@@ -8,6 +8,12 @@ module.exports = function (RED) {
         var node = this;
         node.data = config.data || "";//data
         node.dataType = config.dataType || "msg";
+        node.obj_bbox = config.obj_bbox || "[]";
+        node.obj_bboxType = config.obj_bboxType || "msg";
+        node.obj_conf = config.obj_conf || 0;
+        node.obj_confType = config.obj_confType || "msg";
+        node.obj_label = config.obj_label || "";
+        node.obj_labelType = config.obj_labelType || "msg";
         this.active = config.active;
 
         node.on("input", function (msg) {
@@ -26,7 +32,6 @@ module.exports = function (RED) {
                 node.status({ fill: "red", shape: "dot", text: statusText });
             }
 
-            var data;
             try {
                 /* ****************  Get Image Data Parameter **************** */
                 var dataInput;
@@ -42,11 +47,45 @@ module.exports = function (RED) {
                     nodeStatusError("dataInput is empty (Image parameter)", msg, "Error. Image is null");
                     return null;
                 }
-
                 dataInput = createDataURI(dataInput);
                 
+                /* ****************  Get Object Bounding Box Parameter **************** */
+                var objBbox;
+                RED.util.evaluateNodeProperty(node.obj_bbox, node.obj_bboxType, node, msg, (err, value) => {
+                    if (err) {
+                        nodeStatusError(err, msg, "Error getting Object Bounding Box parameter");
+                        return;//halt flow!
+                    } else {
+                        var tempBbox = value.match(/\d+/g);
+                        tempBbox = (tempBbox !== null ? tempBbox.map(Number) : []);
+                        objBbox = tempBbox;
+                    }
+                });
+
+                /* ****************  Get Object Confidence Detection Box Parameter **************** */
+                var objConf;
+                RED.util.evaluateNodeProperty(node.obj_conf, node.obj_confType, node, msg, (err, value) => {
+                    if (err) {
+                        nodeStatusError(err, msg, "Error getting Object Confidence Detection parameter");
+                        return;//halt flow!
+                    } else {
+                        objConf = parseFloat(value);
+                    }
+                });
+
+                /* ****************  Get Object Label Detection Box Parameter **************** */
+                var objLabel;
+                RED.util.evaluateNodeProperty(node.obj_label, node.obj_labelType, node, msg, (err, value) => {
+                    if (err) {
+                        nodeStatusError(err, msg, "Error getting Label Confidence Detection parameter");
+                        return;//halt flow!
+                    } else {
+                        objLabel = value;
+                    }
+                });
+
                 node.send(msg);//pass it on before displaying
-                RED.comms.publish("sscma-client-video-stream", { id: this.id, data: dataInput });
+                RED.comms.publish("sscma-client-video-stream", { id: this.id, data: dataInput, obj_bbox: objBbox, obj_conf: objConf, obj_label: objLabel});
             }
             catch (e) {
                 nodeStatusError(e, msg, "Error reading image")
