7fc198c58c589362bb2fbe418ffc2fd588b6b147
Muhammad Yunus <muhammadyunusiot@gmail.com>
2024-09-16 13:26:31 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var NODE_PATH = '/sscma-client/';
+
+module.exports = function (RED) {
+    /**
+     * Enable http route to static files
+     */
+    RED.httpAdmin.get(NODE_PATH + 'static/*', function (req, res) {
+        var options = {
+            root: __dirname + '/static/',
+            dotfiles: 'deny'
+        };
+        res.sendFile(req.params[0], options);
+    });
+}
